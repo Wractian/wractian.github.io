32203301c755b2a4defc44125782629c9689d7dc
Wractian <liam.piper@baxter-academy.org>
2017-05-15 13:33:19 -0400

    Buzzz

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -2,10 +2,8 @@ function Game() {
 	var tick=0
 	Game.Game=setInterval(ticks,10)
 	function ticks() {
-		tick++
-		document.getElementById("ticks").innerHTML=tick;
-	}
-}
+		
+	}}
 function tabChange(evt, tabName) {
 	
 	var i, tabcontent, tablinks;
@@ -22,36 +20,3 @@ function tabChange(evt, tabName) {
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";}
 
-function StartProg(evt) {
-	var elem = document.getElementById("bar");
-	var width =0;
-	if (game.id==false){
-		game.id = setInterval(frame,1)
-		function frame() {
-			if (width >=100){
-				clearInterval(game.id);
-				elem.style.width = '0%';
-				game.numberValue+=game.incrementSpeed;
-				
-				game.id=false;
-			}else{
-				width+=game.barSpeed;
-				elem.style.width = width+'%';
-			}
-		}
-	}}
-
-function Upgrade(evt, upgrade) {
-	if ((evt.currentTarget.id=="barSpeed")&&(evt.currentTarget.classList.contains("active"))){
-		game.numberValue-=100*game.barSpeed
-		game.barSpeed=game.barSpeed+.1
-		game.barSpeed=Math.round(game.barSpeed*1000)/1000
-		game.numberValue=Math.floor(game.numberValue)
-	}
-	if ((evt.currentTarget.id=="incrementSpeed")&&(evt.currentTarget.classList.contains("active"))){
-		game.numberValue-=10*game.incrementSpeed
-		game.incrementSpeed=game.incrementSpeed+1
-		game.incrementSpeed=Math.round(game.incrementSpeed*1000)/1000
-		game.numberValue=Math.floor(game.numberValue)
-	}
-}
