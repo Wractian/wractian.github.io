02c079ce75cf5eeb4a9258bc070dae4819d79e03
Wractian <liam.piper@baxter-academy.org>
2017-05-27 19:35:49 -0400

    devtool rewrite

--- Scripts/devtools.js
+++ Scripts/devtools.js
@@ -1,18 +1,24 @@
-function fillBees(evt) {
-    for (var i = 1; i < game.drawer.drawerCounter + 1; i++) {
-        addBee(null, "beeCell" + i)
+function fillBees() {
+    "use strict";
+    var i;
+    for (i = 1; i < game.drawer.drawerCounter + 1; i += 1) {
+        addBee(null, "beeCell" + i);
     }
 }
 
-function deleteBees(evt) {
-    for (var i = 0; i < game.drawer.cells.length; i++) {
-        if (game.drawer.cells[i].firstElementChild.hasChildNodes()) {
-            $(game.drawer.cells[i].firstElementChild.firstElementChild).fadeOut(300, "linear")
-            var id = setTimeout(removeBee, 300, game.drawer.cells[i].firstElementChild);
+function deleteBees() {
+    "use strict";
+    var i, id;
 
-            function removeBee(target) {
-                target.removeChild(target.firstElementChild)
-            }
+    function removeBee(target) {
+        "use strict";
+        target.removeChild(target.firstElementChild);
+    }
+
+    for (i = 0; i < game.drawer.cells.length; i += 1) {
+        if (game.drawer.cells[i].firstElementChild.hasChildNodes()) {
+            $(game.drawer.cells[i].firstElementChild.firstElementChild).fadeOut(300, "linear");
+            setTimeout(removeBee, 300, game.drawer.cells[i].firstElementChild);
         }
     }
 }
