01754784c51f9c204835061680bd68f877549083
Wractian <liam.piper@baxter-academy.org>
2017-05-26 23:10:16 -0400

    Buzz

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -1,10 +1,18 @@
 function Game() {
+	game = {};
+	game.bees={};
+	game.trash={};
 	game.bees.maxBees=100;
-	beeDrawer();
-	var tick=0;
+	game.tick=0;
 	game.drawerCounter=0;
 	game.beeCounter=0;
+	game.trash.trashon=false;
+	game.trash.trashList = [];
+	//DeveloperStuff {
+	beeDrawer();
 	beeBox(null,1)
+	addBee(null,"beeCell1")
+	//DeveloperStuff }
 	game.Game=setInterval(ticks,10);
 	function ticks() {
 	}
@@ -23,8 +31,8 @@ function tabChange(evt, tabName) {
 		tablinks[i].className = tablinks[i].className.replace("active", "");
 	}
 	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";}
-
+	evt.currentTarget.className += " active";
+}
 function beeBox(evt, increment) {
 	var Boxes, Clone, Button
 	var Parent = document.getElementById("MainBox");
@@ -66,6 +74,7 @@ function allowDrop(evt) {
 	evt.preventDefault();
 }
 function drag(evt) {
+
 	evt.dataTransfer.setData("dragid", evt.target.id)
 	evt.dataTransfer.effectAllowed = "copyMove"
 }
@@ -76,27 +85,51 @@ function drop(evt) {
 	evt.target.appendChild(document.getElementById(data))
 	}
 }
-function addBee(evt) {
-	for (var i = 0; i < 10; i++) {
-		
-	
-	var Template = document.getElementsByClassName("template")[1]
-	var Clone = Template.cloneNode(false);
-	Clone.className = Clone.className.replace("template", "");
-	game.beeCounter++
-	Clone.id="bee"+game.beeCounter	
-	document.getElementById("devCell").appendChild(Clone)
-}
+function addBee(evt,elementid) {
+	for (var i = 0; i < 1; i++) {
+		if ($("#"+elementid).hasClass( "bugHolderDiv" )){
+			var Template = document.getElementsByClassName("template")[1]
+			var Clone = Template.cloneNode(false);
+			Clone.className = Clone.className.replace("template", "");
+			game.beeCounter++
+			Clone.id="bee"+game.beeCounter	
+			document.getElementById(elementid).appendChild(Clone)
+		}
+	}
 }
 function beeSelect(evt) {
-	if (evt.currentTarget.firstElementChild.childNodes.length){
-		if (true) {
-			if (evt.currentTarget.style.borderColor=="black")
-			{
-				evt.currentTarget.style.borderColor="#08ca08"
-			}else{
-				evt.currentTarget.style.borderColor="black"
+	var index
+	if (game.trash.trashon) {
+		if (evt.currentTarget.firstElementChild.childNodes.length){
+			if (true) {
+				if (evt.currentTarget.style.backgroundColor=="rgb(241, 241, 241)")
+				{
+					evt.currentTarget.style.backgroundColor="#ef0202";
+					if (!(game.trash.trashList.includes(evt.currentTarget)))
+					game.trash.trashList[game.trash.trashList.length]=evt.currentTarget;
+				}else{
+					if (game.trash.trashList.includes(evt.currentTarget))
+					{
+						index = game.trash.trashList.indexOf(evt.currentTarget)
+						game.trash.trashList.splice(index,1)
+
+					}
+					evt.currentTarget.style.backgroundColor="#f1f1f1";
+				}
 			}
 		}
+	}
+}
+function beeTrash(evt){ 
+	if (game.trash.trashon) {
+		game.trash.trashon=false
+		evt.currentTarget.style.backgroundColor="#f1f1f1"
+		for (var i = 0; i < game.trash.trashList.length; i++) {
+			game.trash.trashList[i].style.backgroundColor="#f1f1f1";
+		}
+		game.trash.trashList=[];
+	}else{
+		evt.currentTarget.style.backgroundColor="#ef0202"
+		game.trash.trashon=true
 	}
 }
