22a20fa32a81ce03aa6d76b4db20a9885eeb36fe
Wractian <liam.piper@baxter-academy.org>
2017-05-27 20:40:48 -0400

    reWriteScripts

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,5 @@
-"use strict";
 function Game() {
+    "use strict";
     game.bees = {};
     game.trash = {};
     game.drawer = {};
@@ -15,19 +15,23 @@ function Game() {
     beeBox(null, 1);
     //DeveloperStuff }
     game.Game = setInterval(ticks, 10);
-    function ticks() {}
+
+    function ticks() {
+        //placeholder
+        game.tick += 1;
+    }
 }
 
 function tabChange(evt, tabName) {
-
-    var i;
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    "use strict";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i += 1) {
         tabcontent[i].style.display = "none";
     }
 
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i += 1) {
         tablinks[i].className = tablinks[i].className.replace("active", "");
     }
     document.getElementById(tabName).style.display = "block";
@@ -35,78 +39,88 @@ function tabChange(evt, tabName) {
 }
 
 function beeBox(evt, increment) {
-    var Boxes, Clone, Button
-    var Parent = document.getElementById("MainBox");
-    var Template = document.getElementsByClassName("template")[0]
-    var Button = document.getElementById("beeButton");
-    var Trash = document.getElementById("drawerTrash")
-    if (increment == 1) {
-        if (!(game.drawer.cells.length == 27)) {
-            game.drawer.drawerCounter++
-                var Clone = Template.firstElementChild.cloneNode(true);
-            Clone.className += " beeCell"
+    "use strict";
+    var Boxes, Clone, Button, Parent, Template, Trash, length, condition;
+    Parent = document.getElementById("MainBox");
+    Template = document.getElementsByClassName("template")[0];
+    Button = document.getElementById("beeButton");
+    Trash = document.getElementById("drawerTrash");
+    if (increment === 1) {
+        condition = (game.drawer.cells.length === 27);
+        if (!condition) {
+            game.drawer.drawerCounter += 1;
+            Clone = Template.firstElementChild.cloneNode(true);
+            Clone.className += " beeCell";
             Clone.childNodes[1].id = "beeCell" + game.drawer.drawerCounter;
             $(Clone).hide();
             Parent.insertBefore(Clone, Parent.firstChild);
             game.drawer.cells.push(Clone);
-            $(Clone).fadeIn(300, "linear")
+            $(Clone).fadeIn(300, "linear");
             $(Button).fadeIn(300, "linear");
             $(Trash).fadeIn(300, "linear");
         }
     } else {
-        var length = game.drawer.cells.length
-        if (!(game.drawer.cells.length == 0)) {
+        length = game.drawer.cells.length;
+        condition = game.drawer.cells.length === 0;
+        if (!condition) {
             Parent.removeChild(game.drawer.cells[length - 1]);
-            game.drawer.cells.splice(game.drawer.cells.length - 1)
-            game.drawer.drawerCounter--
-                if (game.drawer.cells.length == 0) {
-                    $(Button).fadeOut(300, "linear")
-                    $(Trash).fadeOut(300, "linear")
-                }
+            game.drawer.cells.splice(game.drawer.cells.length - 1);
+            game.drawer.drawerCounter += -1;
+            if (game.drawer.cells.length === 0) {
+                $(Button).fadeOut(300, "linear");
+                $(Trash).fadeOut(300, "linear");
+            }
         }
 
     }
 }
 
 function beeDrawer(evt) {
-    var Box = document.getElementById("beeCollapse");
-    var Button = document.getElementById("beeButton");
+    "use strict";
+    var Box, Button;
+    Box = document.getElementById("beeCollapse");
+    Button = document.getElementById("beeButton");
     if (Box.style.maxWidth) {
         Box.style.maxWidth = null;
-        Button.innerHTML = "&laquo;"
+        Button.innerHTML = "&laquo;";
     } else {
-        Box.style.maxWidth = 100 + "%"
-        Button.innerHTML = "&raquo;"
+        Box.style.maxWidth = 100 + "%";
+        Button.innerHTML = "&raquo;";
     }
 }
 
 function allowDrop(evt) {
+    "use strict";
     evt.preventDefault();
 }
 
 function drag(evt) {
-
-    evt.dataTransfer.setData("dragid", evt.target.id)
-    evt.dataTransfer.effectAllowed = "copyMove"
+    "use strict";
+    evt.dataTransfer.setData("dragid", evt.target.id);
+    evt.dataTransfer.effectAllowed = "copyMove";
 }
 
 function drop(evt) {
+    "use strict";
+    var data;
     evt.preventDefault();
-    if ((evt.target.className == "bugHolderDiv")) {
-        var data = evt.dataTransfer.getData("dragid");
-        evt.target.appendChild(document.getElementById(data))
+    if (evt.target.className === "bugHolderDiv") {
+        data = evt.dataTransfer.getData("dragid");
+        evt.target.appendChild(document.getElementById(data));
     }
 }
 
 function addBee(evt, elementid) {
-    for (var i = 0; i < 1; i++) {
+    "use strict";
+    var i, Template, Clone;
+    for (i = 0; i < 1; i += 1) {
         if ($("#" + elementid).hasClass("bugHolderDiv")) {
-            if (($("#" + elementid).children().length == 0) || (elementid == "devCell")) {
-                var Template = document.getElementsByClassName("template")[1];
-                var Clone = Template.cloneNode(false);
+            if (($("#" + elementid).children().length === 0) || (elementid === "devCell")) {
+                Template = document.getElementsByClassName("template")[1];
+                Clone = Template.cloneNode(false);
                 Clone.className = Clone.className.replace("template", " bee");
-                game.beeCounter++
-                    Clone.id = "bee" + game.beeCounter;
+                game.beeCounter += 1;
+                Clone.id = "bee" + game.beeCounter;
                 $(Clone).hide();
                 document.getElementById(elementid).appendChild(Clone);
                 $(Clone).fadeIn(300, "linear");
@@ -116,18 +130,20 @@ function addBee(evt, elementid) {
 }
 
 function beeSelect(evt) {
-    var index
+    "use strict";
+    var index;
     if (game.trash.trashon) {
         if (evt.currentTarget.firstElementChild.childNodes.length) {
             if (true) {
-                if (evt.currentTarget.style.backgroundColor == "rgb(241, 241, 241)") {
+                if (evt.currentTarget.style.backgroundColor === "rgb(241, 241, 241)") {
                     evt.currentTarget.style.backgroundColor = "#ef0202";
-                    if (!(game.trash.trashList.includes(evt.currentTarget)))
-                        game.trash.trashList.push(evt.currentTarget)
+                    if (!(game.trash.trashList.includes(evt.currentTarget))) {
+                        game.trash.trashList.push(evt.currentTarget);
+                    }
                 } else {
                     if (game.trash.trashList.includes(evt.currentTarget)) {
-                        index = game.trash.trashList.indexOf(evt.currentTarget)
-                        game.trash.trashList.splice(index, 1)
+                        index = game.trash.trashList.indexOf(evt.currentTarget);
+                        game.trash.trashList.splice(index, 1);
 
                     }
                     evt.currentTarget.style.backgroundColor = "#f1f1f1";
@@ -138,30 +154,32 @@ function beeSelect(evt) {
 }
 
 function beeTrash(evt) {
-    if (game.trash.trashon) {
-        game.trash.trashon = false
-        evt.currentTarget.style.backgroundColor = "#f1f1f1"
-        for (vari = 0; i < game.trash.trashList.length; i++) {
-            var element = game.trash.trashList[i]
-            element.style.backgroundColor = "#f1f1f1";
-            $(element.firstElementChild.firstElementChild).fadeOut(300, "linear")
-            var id = setTimeout(removeBee, 300, element.firstElementChild);
+    "use strict";
+    var element, i, bees;
 
-            function removeBee(target) {
-                target.removeChild(target.firstElementChild)
-                console.log("test")
-            }
+    function removeBee(target) {
+        target.removeChild(target.firstElementChild);
+        console.log("test");
+    }
+    if (game.trash.trashon) {
+        game.trash.trashon = false;
+        evt.currentTarget.style.backgroundColor = "#f1f1f1";
+        for (i = 0; i < game.trash.trashList.length; i += 1) {
+            element = game.trash.trashList[i];
+            element.style.backgroundColor = "#f1f1f1";
+            $(element.firstElementChild.firstElementChild).fadeOut(300, "linear");
+            setTimeout(removeBee, 300, element.firstElementChild);
         }
-        var bees = document.getElementsByClassName("bee")
-        for (var i = 0; i < bees.length; i++) {
+        bees = document.getElementsByClassName("bee");
+        for (i = 0; i < bees.length; i += 1) {
             bees[i].setAttribute('draggable', 'true');
         }
         game.trash.trashList = [];
     } else {
-        evt.currentTarget.style.backgroundColor = "#ef0202"
-        game.trash.trashon = true
-        var bees = document.getElementsByClassName("bee")
-        for (var i = 0; i < bees.length; i++) {
+        evt.currentTarget.style.backgroundColor = "#ef0202";
+        game.trash.trashon = true;
+        bees = document.getElementsByClassName("bee");
+        for (i = 0; i < bees.length; i += 1) {
             bees[i].setAttribute('draggable', 'false');
         }
     }
