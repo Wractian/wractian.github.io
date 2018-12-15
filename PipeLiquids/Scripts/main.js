566197961e36f79869dfe7b6ce6fc0b0cfcaa0b3
Wractian <liam.piper@baxter-academy.org>
2018-12-15 02:18:00 -0500

    new changes, icon+other stuff

--- PipeLiquids/Scripts/main.js
+++ PipeLiquids/Scripts/main.js
@@ -14,8 +14,8 @@ var __extends = (this && this.__extends) || (function () {
 })();
 var Vector2D = /** @class */ (function () {
     function Vector2D(x, y) {
-        this.x = x;
-        this.y = y;
+        this.x = x || 0;
+        this.y = y || 0;
     }
     Vector2D.prototype.toArray = function () {
         var retarr = [];
@@ -37,7 +37,7 @@ var Basic_Machine = /** @class */ (function (_super) {
     function Basic_Machine(pos, size, texture) {
         var _this = _super.call(this, pos) || this;
         _this.size = size;
-        _this.texture = texture;
+        _this.texture = texture || "/Content/DefaultTexture.png";
         return _this;
     }
     return Basic_Machine;
@@ -47,7 +47,7 @@ var Complex_Machine = /** @class */ (function (_super) {
     function Complex_Machine(pos, size, texture) {
         var _this = _super.call(this, pos) || this;
         _this.size = size;
-        _this.texture = texture;
+        _this.texture = texture || "/Content/DefaultTexture.png";
         return _this;
     }
     return Complex_Machine;
@@ -55,15 +55,124 @@ var Complex_Machine = /** @class */ (function (_super) {
 var Basic_Distiller = /** @class */ (function (_super) {
     __extends(Basic_Distiller, _super);
     function Basic_Distiller(pos, size, texture) {
-        return _super.call(this, pos, size, texture) || this;
+        var _this = _super.call(this, pos, size, texture) || this;
+        _this.tank = new FluidTank(Basic_Distiller.capacitymax);
+        return _this;
     }
+    Basic_Distiller.prototype.Insert = function (flu, cap) {
+        return this.tank.Insert(flu, cap);
+    };
+    Basic_Distiller.capacitymax = 100;
     return Basic_Distiller;
 }(Basic_Machine));
-Basic_Distiller.prototype.capacity = 0;
+var Fluid = /** @class */ (function () {
+    function Fluid(fluid) {
+        this.fluidtype = fluid || "Empty";
+    }
+    Fluid.Empty = new Fluid();
+    return Fluid;
+}());
+var FluidTank = /** @class */ (function () {
+    function FluidTank(cap) {
+        this.capacity = 0;
+        this.maxcapacity = cap;
+        this.fluid = Fluid.Empty;
+    }
+    FluidTank.prototype.clear = function () {
+        this.fluid = Fluid.Empty;
+        this.capacity = 0;
+    };
+    FluidTank.prototype.isEmpty = function () {
+        if (this.fluid == Fluid.Empty) {
+            return true;
+        }
+        return false;
+    };
+    FluidTank.prototype.Insert = function (flu, cap) {
+        if (this.isEmpty) {
+            this.fluid = flu; //Sets fluid type if tank currently empty
+        }
+        if (this.fluid != flu) {
+            return 0;
+        }
+        this.capacity = this.capacity + cap;
+        if (this.capacity > this.maxcapacity) {
+            var val = this.capacity - this.maxcapacity;
+            this.capacity = this.maxcapacity;
+            return cap - val;
+        }
+        return cap;
+    };
+    /**
+     * Transfer;
+     * BETA FUNCTION JUST FOR TESTING
+     */
+    FluidTank.prototype.Transfer = function (tank, num) {
+        if (this.capacity - num < 0) { //Fixes too much num
+            num = this.capacity;
+        }
+        this.capacity = this.capacity - tank.Insert(this.fluid, num);
+        console.log(this.capacity);
+        if (this.capacity == 0) {
+            this.fluid = Fluid.Empty;
+        }
+    };
+    /**
+     * printVal;
+     * Used to print the capacity+fluidtype
+     */
+    FluidTank.prototype.printVal = function () {
+        if (this.isEmpty()) {
+            console.log("This fluid tank is empty");
+            return;
+        }
+        console.log("This fluid tank contains " + this.capacity + " units of " + this.fluid.fluidtype);
+    };
+    return FluidTank;
+}());
+var Pipe = /** @class */ (function () {
+    function Pipe() {
+        this.tank = new FluidTank(96);
+    }
+    return Pipe;
+}());
+function tickpipes() {
+    var processarr = [];
+    //First create array of transfer materials
+    processarr[processarr.length] = { target: 0, reciver: 1, amount: pipes[0].tank.capacity / 2 }; //A-B
+    processarr[processarr.length] = { target: 1, reciver: 0, amount: pipes[1].tank.capacity / 2 / 2 }; //B-A
+    processarr[processarr.length] = { target: 1, reciver: 2, amount: pipes[1].tank.capacity / 2 / 2 }; //B-C
+    processarr[processarr.length] = { target: 2, reciver: 1, amount: pipes[2].tank.capacity / 2 }; //C-B
+    //Now transfer
+    for (var i = 0; i < processarr.length; i++) {
+        pipes[processarr[i].target].tank.Transfer(pipes[processarr[i].reciver].tank, processarr[i].amount);
+    }
+    console.table([pipes[0].tank, pipes[1].tank, pipes[2].tank]);
+}
+var pipes = [];
+for (var i = 0; i < 3; i++) {
+    pipes[i] = new Pipe();
+}
 var y = new Vector2D(1, 2);
-var m = new Basic_Machine(y, y, "hit or miss");
-console.log(m);
-var u = [y, new Vector2D(2, 3)];
-var c = new Complex_Machine(y, u, "hit or miss");
+//var m = new Basic_Machine(y, y);
+//console.log(m);
+/*var u: Vector2D[] = [y, new Vector2D(2, 3)];
+var c = new Complex_Machine(y, u);
 console.log(c);
+*/
+var gx = new Basic_Distiller(y, y);
+gx.Insert(new Fluid("Apple sauce"), 55);
+gx.tank.printVal();
+var gy = new Basic_Distiller(y, y);
+function redrawdistillers() {
+    console.clear();
+    console.log("GX");
+    console.table(gx);
+    console.table(gx.tank);
+    console.log("GY");
+    console.table(gy);
+    console.table(gy.tank);
+}
+;
+redrawdistillers();
 //# sourceMappingURL=main.js.map
--- PipeLiquids/Scripts/main.ts
+++ PipeLiquids/Scripts/main.ts
@@ -4,70 +4,195 @@
 class Vector2D {
     x: number;
     y: number;
-    constructor(x: number,y: number) {
-        this.x = x;
-        this.y = y;
+    constructor(x?: number, y?: number) {
+        this.x = x || 0;
+        this.y = y || 0;
     }
 
-    toArray(){
+    public toArray() {
         let retarr = [];
         retarr[0] = this.x;
         retarr[1] = this.y;
-        return(retarr);
+        return (retarr);
     }
-
 };
+
 
 
 
 class Machine {
     position: Vector2D;
-    constructor(pos: Vector2D){
+    constructor(pos: Vector2D) {
         this.position = pos;
     }
 }
 
 class Basic_Machine extends Machine {
     size: Vector2D;
-    texture : string;
-    constructor(pos: Vector2D, size: Vector2D, texture: string){
+    texture: string;
+    constructor(pos: Vector2D, size: Vector2D, texture?: string) {
         super(pos);
         this.size = size;
-        this.texture = texture;
+        this.texture = texture || "/Content/DefaultTexture.png";
     }
 }
 
 class Complex_Machine extends Machine {
     size: Vector2D[];
-    texture : string;
-    constructor(pos: Vector2D, size: Vector2D[], texture: string){
+    texture: string;
+    constructor(pos: Vector2D, size: Vector2D[], texture?: string) {
         super(pos);
         this.size = size;
-        this.texture = texture;
+        this.texture = texture || "/Content/DefaultTexture.png";
     }
 }
 
-class Basic_Distiller extends Basic_Machine{
-    
-    
-    constructor(pos: Vector2D, size: Vector2D, texture: string){
-        super(pos,size,texture)
+class Basic_Distiller extends Basic_Machine {
+
+    static capacitymax = 100;
+    tank: FluidTank;
+    currentcapacity: number;
+    constructor(pos: Vector2D, size: Vector2D, texture?: string) {
+        super(pos, size, texture);
+        this.tank = new FluidTank(Basic_Distiller.capacitymax);
+    }
+    public Insert(flu: Fluid, cap: number): number {
+        return this.tank.Insert(flu, cap);
     }
 
 }
-interface Basic_Distiller {
-    capacity: number;
+
+class Fluid {
+    fluidtype: String;
+    static Empty = new Fluid();
+    constructor(fluid?: String) {
+        this.fluidtype = fluid || "Empty";
+    }
+
 }
-Basic_Distiller.prototype.capacity = 0;
+
+class FluidTank {
+    maxcapacity: number;
+    fluid: Fluid;
+    capacity: number = 0;
+    constructor(cap: number) {
+        this.maxcapacity = cap;
+        this.fluid = Fluid.Empty;
+    }
+    public clear(): void {
+        this.fluid = Fluid.Empty;
+        this.capacity = 0;
+    }
+    public isEmpty(): boolean {
+        if (this.fluid == Fluid.Empty) {
+            return true;
+        }
+        return false;
+    }
+    public Insert(flu: Fluid, cap: number): number {
+        if (this.isEmpty) {
+            this.fluid = flu; //Sets fluid type if tank currently empty
+        }
+        if (this.fluid != flu) {
+            return 0;
+        }
+
+        this.capacity = this.capacity + cap;
+        if (this.capacity > this.maxcapacity) {
+            var val = this.capacity - this.maxcapacity;
+            this.capacity = this.maxcapacity;
+            return cap - val;
+        }
+        return cap;
+    }
+
+    /**
+     * Transfer;
+     * BETA FUNCTION JUST FOR TESTING
+     */
+    public Transfer(tank: FluidTank, num: number) {
+
+        if (this.capacity - num < 0) { //Fixes too much num
+            num = this.capacity;
+        }
+        this.capacity = this.capacity - tank.Insert(this.fluid, num);
+        console.log(this.capacity);
+        if (this.capacity == 0) {
+            this.fluid = Fluid.Empty;
+        }
+    }
+    /**
+     * printVal;
+     * Used to print the capacity+fluidtype
+     */
+    public printVal(): void {
+        if (this.isEmpty()) {
+            console.log(`This fluid tank is empty`);
+            return;
+        }
+        console.log(`This fluid tank contains ${this.capacity} units of ${this.fluid.fluidtype}`);
+    }
+}
+
+class Pipe {
+
+    tank: FluidTank = new FluidTank(96);
+
+    constructor() {
+
+    }
+
+}
+
+
+function tickpipes() {
+    var processarr: any = [];
 
 
 
+    //First create array of transfer materials
 
-var y = new Vector2D(1,2);
-var m = new Basic_Machine(y,y,"hit or miss");
-console.log(m);
+    processarr[processarr.length] = {target:0, reciver:1, amount: pipes[0].tank.capacity/2}; //A-B
+    processarr[processarr.length] = {target:1, reciver:0, amount: pipes[1].tank.capacity/2/2}; //B-A
+    processarr[processarr.length] = {target:1, reciver:2, amount: pipes[1].tank.capacity/2/2}; //B-C
+    processarr[processarr.length] = {target:2, reciver:1, amount: pipes[2].tank.capacity/2}; //C-B
+    
+    //Now transfer
+    for (let i = 0; i < processarr.length; i++) {
+        pipes[processarr[i].target].tank.Transfer(pipes[processarr[i].reciver].tank,processarr[i].amount);
+    }
 
-var u: Vector2D[] = [y, new Vector2D(2,3)];
-var c = new Complex_Machine(y,u,"hit or miss");
+    console.table([pipes[0].tank,pipes[1].tank,pipes[2].tank])
+
+}
+
+var pipes: Pipe[] = []
+for (let i = 0; i < 3; i++) {
+    pipes[i] = new Pipe();
+}
+
+
+var y = new Vector2D(1, 2);
+//var m = new Basic_Machine(y, y);
+//console.log(m);
+
+/*var u: Vector2D[] = [y, new Vector2D(2, 3)];
+var c = new Complex_Machine(y, u);
 console.log(c);
+*/
 
+var gx = new Basic_Distiller(y, y)
+gx.Insert(new Fluid("Apple sauce"), 55)
+gx.tank.printVal();
+var gy = new Basic_Distiller(y, y)
+
+function redrawdistillers() {
+    console.clear();
+    console.log("GX");
+    console.table(gx);
+    console.table(gx.tank);
+    console.log("GY");
+    console.table(gy);
+    console.table(gy.tank);
+};
+redrawdistillers();
