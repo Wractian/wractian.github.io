451de657140c9ed9879d1aff19cb13bc51802579
Wractian <liam.piper@baxter-academy.org>
2019-02-11 22:16:16 -0500

    keyboard

--- TurnBased/Scripts/script.js
+++ TurnBased/Scripts/script.js
@@ -3,22 +3,23 @@ class Character {
     Moves = [];
     Statuses = [];
     Items = [];
-
+    x = 0;
+    y = 0;
     constructor(name, health, sanity) {
         this.name = name;
         this.health = health;
         this.sanity = sanity;
     }
 
-    useMove(num,target) {
+    useMove(num, target) {
         this.Moves[num].useMove(target);
     }
-    
+
     tickStatuses() {
         for (let i = 0; i < this.Statuses.length; i++) {
             this.Statuses[i].tick(this);
             if (this.Statuses[i].duration <= 0) {
-                this.Statuses.splice(i,1)
+                this.Statuses.splice(i, 1)
             }
         }
     }
@@ -29,20 +30,20 @@ class Character {
         }
     }
 
-    addMove(move){
+    addMove(move) {
         this.Moves.push(move);
     }
 
-    addStatus(status){
+    addStatus(status) {
         this.Statuses.push(status);
     }
 
-    addItem(item){
+    addItem(item) {
         this.Items.push(item);
     }
 
     //Dev Functions
-    logItemList(){
+    logItemList() {
         console.table(this.Items);
     }
 };
@@ -50,14 +51,14 @@ class Character {
 
 class Status {
 
-    constructor(name, duration, tickfunc){
+    constructor(name, duration, tickfunc) {
         this.name = name ? name : "unnamed status";
-        this.duration = duration ? duration: 1;
-        this.tickfunc = tickfunc ? tickfunc : function(){};
-        
+        this.duration = duration ? duration : 1;
+        this.tickfunc = tickfunc ? tickfunc : function () { };
+
     }
 
-    tick(char){
+    tick(char) {
         this.tickfunc(char);
         this.duration = this.duration - 1;
     }
@@ -66,11 +67,11 @@ class Status {
 
 class Bleed extends Status {
 
-    constructor(duration,strength){
+    constructor(duration, strength) {
         duration = duration ? duration : 5;
         strength = strength ? strength : 1;
-    
-        super("Bleed", duration, function(char){char.health = char.health - strength})
+
+        super("Bleed", duration, function (char) { char.health = char.health - strength })
     }
 
 }
@@ -81,7 +82,7 @@ class Move {
     constructor(name, damage, func) {
         this.name = name ? name : "unnamed move";
         this.damage = damage ? damage : 1;
-        this.usefunc = func ? func : function(){};
+        this.usefunc = func ? func : function () { };
     }
     useMove(target) {
         console.log(`Using move ${this.name}`);
@@ -89,49 +90,62 @@ class Move {
     }
 }
 
-class Item { 
+class Item {
     constructor(name, id, sprite, desc, func) {
         this.name = name ? name : "unnamed item";
-        this.id = id ? id: 0;
+        this.id = id ? id : 0;
         this.sprite = sprite ? sprite : "default";
         this.desc = desc ? desc : "This item has no description";
-        this.tickfunc = func ? func : function(){};
+        this.tickfunc = func ? func : function () { };
     }
-    
-    tick(char){
+
+    tick(char) {
         this.tickfunc(char);
     }
 
     get info() {
-        return [this.name,this.desc]
+        return [this.name, this.desc]
     }
 }
 
 
-function onresize(){
-    var desiredAspectRatio = 4/3
+function onresize() {
+    var desiredAspectRatio = 4 / 3
     console.log()
 
 }
-window.addEventListener('resize' , onresize)
+window.addEventListener('resize', onresize)
 
+keys = [];
+
+function keyboardHandler(e) {
+    if(e.type == "keydown"){
+        keys[e.key] = true;
+    }
+    if(e.type == "keyup"){
+        keys[e.key] = false;
+    }
+}
+window.addEventListener("keydown", keyboardHandler, false);
+window.addEventListener("keypress", keyboardHandler, false);
+window.addEventListener("keyup", keyboardHandler, false);
 
 x = 0;
 var prevtime,
-fpscap = 30;
+    fpscap = 30;
 fpsarr = [];
 
 var imagesource = document.createElement("img")
 imagesource.src = "Content/Sprites/test.png"
 
-function draw(time){
+function draw(time) {
     window.requestAnimationFrame(draw);
     //Deals with fps stuff
     var timediff = time - prevtime;
-    if(timediff < 1000 / fpscap){
+    if (timediff < 1000 / fpscap) {
         return;
     }
-    if(fpsarr.length >= 30){
+    if (fpsarr.length >= 30) {
         fpsarr.shift();
     }
     fpsarr.push(timediff);
@@ -140,29 +154,39 @@ function draw(time){
         sum = sum + fpsarr[i];
     }
     sum = sum / fpsarr.length;
-    sum = Math.trunc((1/sum) * 1000)
+    sum = Math.trunc((1 / sum) * 1000);
     document.getElementById("fpsmeter").innerHTML = sum;
 
-
     //Actual draw event
+    var playerspeed = 2
+
+    if(keys["w"]||keys["ArrowUp"]){
+        ch.y -= playerspeed;
+    }
+    if(keys["s"]||keys["ArrowDown"]){
+        ch.y += playerspeed;
+    }
+    if(keys["a"]||keys["ArrowLeft"]){
+        ch.x -= playerspeed;
+    }
+    if(keys["d"]||keys["ArrowRight"]){
+        ch.x += playerspeed;
+    }
 
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath()
-    context.arc(x,Math.sin(x/4)*10+60,40,0,2*Math.PI)
+    context.arc(x, Math.floor(Math.sin(x / 4) * 10 + 60), 40, 0, 2 * Math.PI)
     context.stroke()
-    context.drawImage(imagesource,40,40)
+    context.drawImage(imagesource, ch.x, ch.y);
 
-    x+=.5;
-    prevtime = time
-
-
-
+    x += 1;
+    prevtime = time;
 }
 
 ch = new Character("Liam", 10, 10);
-ch.addMove(new Move("Slice",10,function(e){console.log(e)}));
-ch.addMove(new Move("Rend",9,function(char){char.addStatus(new Bleed(5,1))}));
-ch.addItem(new Item("Health Orb",0,"/Content/Orb.png","An orb that restores HP at the cost of sanity", function(char){char.health++;char.sanity--}));
+ch.addMove(new Move("Slice", 10, function (e) { console.log(e) }));
+ch.addMove(new Move("Rend", 9, function (char) { char.addStatus(new Bleed(5, 1)) }));
+ch.addItem(new Item("Health Orb", 0, "/Content/Orb.png", "An orb that restores HP at the cost of sanity", function (char) { char.health++; char.sanity-- }));
 
 
 
