001a8d6c838f1636f1f535936bd4bc120ecac3f6
Wractian <liam.piper@baxter-academy.org>
2019-02-19 02:07:09 -0500

    commiting

--- TurnBased/Scripts/script.js
+++ TurnBased/Scripts/script.js
@@ -2,6 +2,8 @@
 //imports
 import Utils from "./Utils.js";
 
+
+//classdefs
 class Character {
     constructor(name, health, sanity) {
         this.name = name;
@@ -150,7 +152,7 @@ class Tileset {
     }
 }
 
-
+//functions
 function onresize() {
     var desiredAspectRatio = 4 / 3;
 }
@@ -158,7 +160,7 @@ window.addEventListener("resize", onresize);
 
 var keys = [];
 
-//add keydown to array, pop keyad
+//add keydown to array, pop keyup
 
 function keyboardHandler(e) {
     console.log(e)
