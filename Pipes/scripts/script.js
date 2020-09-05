3f35292f1fef0c14dd345ea710e11223c7f11d98
Vivian Piper <vivian.piper134@gmail.com>
2020-09-05 15:27:47 -0400

    egg

---
+++ Pipes/scripts/script.js
@@ -0,0 +1,30 @@
+//Dynamically sized grid using "advanced alpha masking type programing"
+
+
+
+
+
+class grid{
+    constructor(){
+        
+    }
+    /**
+     * Prints grid to console
+     */
+    consoleGrid(){
+        
+    }
+}
+
+class customGrid extends grid{
+    constructor(){
+        super();
+    }
+}
+
+
+
+
+
+
+Grid = new grid() 
